e489cd59b6a838ded8228e3c48c8db0db468e7d0
Derek Haus <rumbleincybertron@gmail.com>
2019-03-23 16:37:55 +0900

    Sandbox NavMesh
    Added NavMesh and mover script for moving the player to a target.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent playerNavMeshAgent;
+
+    void Start()
+    {
+        playerNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        playerNavMeshAgent.SetDestination(target.position);
+    }
+}
