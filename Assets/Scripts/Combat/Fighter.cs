df207e34c536154b990d868fa8b3b031d8bdf4ab
Derek <RumbleInCybertron@users.noreply.github.com>
2019-06-01 21:35:48 +0200

    Throttling Attacks

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
+using System;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0f;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;             // TODO Remove later: Keeps player from having to hold down the mouse button to move
 
             if (!GetIsInRange())
@@ -21,7 +26,16 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehavior();
+            }
+        }
+
+        private void AttackBehavior()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
                 GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
             }
         }
 
