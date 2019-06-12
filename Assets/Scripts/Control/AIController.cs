f05ea53bb140e4034d22d3cbc0aed03664658858
Derek <RumbleInCybertron@users.noreply.github.com>
2019-06-12 17:26:43 +0200

    AI Chase Distance Calculation
    The enemy is told to chase the player when the player gets within the chaseDistance.

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+
+            if (DistanceToPlayer() <= chaseDistance)
+            {
+                print(gameObject.name + ": Chase the Player!");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
