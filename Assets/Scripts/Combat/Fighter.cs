4f739830220e21b4c4d532288be9f81585faa594
Derek <RumbleInCybertron@users.noreply.github.com>
2019-06-12 15:58:18 +0200

    Ignore Dead Enemies
    Check whether or not the player can attack an enemie i.e. is the enemy already dead?

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -55,12 +55,20 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
+        public bool CanAttack(CombatTarget combatTarget)
+        {
+            if (combatTarget == null) { return false; }
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return targetToTest != null && !targetToTest.IsDead();
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
         }
-    
+
+          
         public void Cancel()
         {
             GetComponent<Animator>().SetTrigger("stopAttack");
