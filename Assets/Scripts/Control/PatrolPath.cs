f72260409929506b6dab8de01cc6eb5dfd239135
Derek <RumbleInCybertron@users.noreply.github.com>
2019-06-17 17:18:46 +0200

    Waypoint Visualization
    Used gizmos to create a sphere represenation of child waypoints.

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                const float waypointGizmoRadius = .2f;
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+            }
+        }
+    }
+}
