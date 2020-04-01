e8d47df016e43234ecd01c568a96db93d2cce24c
Derek <RumbleInCybertron@users.noreply.github.com>
2020-04-01 21:29:48 +0200

    Avoiding Singleton Pattern
    In order to avoid having many components or classes depend on one thing, did not use a singleton. Did, however, use a global variable as Unity is limited in its functionality.

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -10,19 +10,9 @@ namespace RPG.SceneManagement
         private void Start()
         {
             canvasGroup = GetComponent<CanvasGroup>();
-
-            StartCoroutine(FadeOutIn());
         }
 
-        IEnumerator FadeOutIn()
-        {
-            yield return FadeOut(3f);
-            print("Faded out");
-            yield return FadeIn(1.5f);
-            print("Faded in");
-        }
-
-        IEnumerator FadeOut(float time)
+        public IEnumerator FadeOut(float time)
         {
             while(canvasGroup.alpha < 1)
             {
@@ -31,7 +21,7 @@ namespace RPG.SceneManagement
             }
         }
 
-        IEnumerator FadeIn(float time)
+        public IEnumerator FadeIn(float time)
         {
             while (canvasGroup.alpha > 0)
             {
